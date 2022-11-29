9917576cd0263750c07f22d7cb41b8b841392cc7
Julian Rodriguez <julianguerra92@gmail.com>
2022-11-29 07:52:32 -0500

    Ajuste de aplicación para su despliegue en la nube

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const { dbConnection } = require('./database/config');
+require('dotenv').config();
 
 
 //Crear el servidor/aplicación de express
@@ -22,6 +24,11 @@ app.use(express.json());
 
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+//Manejar demas rutas
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 //Definición de la dirección de acceso al servidor
 app.listen(process.env.PORT, () => {
